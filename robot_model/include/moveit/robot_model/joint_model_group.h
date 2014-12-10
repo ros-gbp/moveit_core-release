1588594dd03638c6632aa47df02f189e744ea9a6
Michael Ferguson <mfergs7@gmail.com>
2014-12-09 21:57:32 -0800

    Rebase from 'debian/hydro/moveit_core'

--- robot_model/include/moveit/robot_model/joint_model_group.h
+++ robot_model/include/moveit/robot_model/joint_model_group.h
@@ -409,6 +409,9 @@ public:
     return subgroup_names_;
   }
 
+  /** \brief Get the groups that are subsets of this one (in terms of joints set) */
+  void getSubgroups(std::vector<const JointModelGroup*>& sub_groups) const;
+
   /** \brief Check if the joints of group \e group are a subset of the joints in this group */
   bool isSubgroup(const std::string& group) const
   {
@@ -464,7 +467,23 @@ public:
   {
     return attached_end_effector_names_;
   }
-  
+
+  /**
+   * \brief Get a vector of end effector tips included in a particular joint model group as defined by the SRDF end effector semantic
+   *        e.g. for a humanoid robot this would return 4 tips for the hands and feet
+   * \param tips - the output vector of link models of the tips
+   * \return true on success
+   */
+  bool getEndEffectorTips(std::vector<const LinkModel*> &tips) const;
+
+  /**
+   * \brief Get a vector of end effector tips included in a particular joint model group as defined by the SRDF end effector semantic
+   *        e.g. for a humanoid robot this would return 4 tips for the hands and feet
+   * \param tips - the output vector of link names of the tips
+   * \return true on success
+   */
+  bool getEndEffectorTips(std::vector<std::string> &tips) const;
+
   /** \brief Get the bounds for all the active joints */
   const JointBoundsVector& getActiveJointModelsBounds() const
   {
